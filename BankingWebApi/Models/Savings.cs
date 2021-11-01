b9e3f695a70625d004af8570139fbbab548039e0
Elyse Roth <elysemorganroth@gmail.com>
2021-11-01 14:52:53 -0400

    created savings branch and controller added virtual instance of savings to customer to create FK

---
+++ BankingWebApi/Models/Savings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingWebApi.Models
+{
+    public class Savings : Account
+    {
+        public decimal InterestRate { get; set; } = 0.02m;
+        public int CustomerId { get; set; }
+        public virtual Customer customer { get; set; }
+
+        public decimal CalculateAndPayInterest (int months)
+        {
+            var interest = Balance * (InterestRate / 12) * months;
+            Deposit(interest);
+            return interest;
+
+        }
+
+
+
+    }
+}
