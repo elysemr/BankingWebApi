d617c230c96c9ef3cc7dea9a28ec925639c51a08
Elyse Roth <elysemorganroth@gmail.com>
2021-11-01 14:58:21 -0400

    added decimal attribute to savings

--- BankingWebApi/Models/Savings.cs
+++ BankingWebApi/Models/Savings.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BankingWebApi.Models
 {
     public class Savings : Account
-    {
+    { 
+        [Column(TypeName = "decimal(11,2)")]
         public decimal InterestRate { get; set; } = 0.02m;
         public int CustomerId { get; set; }
         public virtual Customer customer { get; set; }
