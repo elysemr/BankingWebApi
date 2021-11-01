017fb95c5236021ced39fc9e9b0efdda498c65f6
Elyse Roth <elysemorganroth@gmail.com>
2021-11-01 10:37:43 -0400

    Add project files.

---
+++ BankingWebApi/Models/BankDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingWebApi.Models
+{
+    public class BankDbContext : DbContext
+    {
+        public virtual DbSet<Customer> Customers { get; set; }
+
+
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) { }
+    }
+}
