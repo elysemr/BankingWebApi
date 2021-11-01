d39f4c0425b4f21f8761797b40cbcb1767642733
Parth Patel <parth2002715@gmail.com>
2021-11-01 15:24:26 -0400

    Credit card additions

---
+++ BankingWebApi/Controllers/CreditCardsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BankingWebApi.Models;
+
+namespace BankingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardsController : ControllerBase
+    {
+        private readonly BankDbContext _context;
+
+        public CreditCardsController(BankDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CreditCards
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CreditCard>>> GetCreditCards()
+        {
+            return await _context.CreditCards.ToListAsync();
+        }
+
+        // GET: api/CreditCards/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CreditCard>> GetCreditCard(int id)
+        {
+            var creditCard = await _context.CreditCards.FindAsync(id);
+
+            if (creditCard == null)
+            {
+                return NotFound();
+            }
+
+            return creditCard;
+        }
+
+        // PUT: api/CreditCards/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCreditCard(int id, CreditCard creditCard)
+        {
+            if (id != creditCard.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(creditCard).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CreditCardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CreditCards
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CreditCard>> PostCreditCard(CreditCard creditCard)
+        {
+            _context.CreditCards.Add(creditCard);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCreditCard", new { id = creditCard.Id }, creditCard);
+        }
+
+        // DELETE: api/CreditCards/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCreditCard(int id)
+        {
+            var creditCard = await _context.CreditCards.FindAsync(id);
+            if (creditCard == null)
+            {
+                return NotFound();
+            }
+
+            _context.CreditCards.Remove(creditCard);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CreditCardExists(int id)
+        {
+            return _context.CreditCards.Any(e => e.Id == id);
+        }
+        private static void PayBalance(CreditCard card, int num)
+        {
+            if (card.Date.AddMonths(1) <= DateTime.Today)
+            {
+                card.Balance += card.intrestowed;
+                card.Balance -= num;
+            }
+            card.Balance -= num;
+        }
+    }
+}
